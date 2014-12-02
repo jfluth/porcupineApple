e9e2f825d161de5004dad5e4e34e4d68f31cdb0c
Paul Long <github@thelongs.ws>
2014-12-01 23:20:38 -0800

    Implemented printing the Logo and blank tile functionality

--- hdl/video_subsystem/paint_screen.v
+++ hdl/video_subsystem/paint_screen.v
@@ -8,7 +8,7 @@
 // Module Name: paint_screen
 //
 // Description: 
-// 
+// _PWL NO RESET IMPLEMENTED!!!!!!!!!!!!!
 //
 // Revision:
 // Revision 0.01 - File Created
@@ -18,18 +18,21 @@
 
 
 module paint_screen(
+	input             clk,
+	input             rst,
     input      [9:0]  pixel_x,
     input      [9:0]  pixel_y,
     input             vid_on,
     input      [3:0]  tile_data,
     input      [11:0] icon_data,
     output reg [7:0]  tile_address,
-    output reg [20:0] screen_address,
-    output reg [11:0] icon_address,
-    output reg [2:0]  ROM_select
+    output reg [9:0]  icon_address,
+	output reg [19:0] screen_address,
+    output reg [2:0]  ROM_select,
+	output reg [11:0] screen_color
     );
     
-    // break screen into named aread
+    // break screen into named regions
     localparam [3:0] TOP          = 4'd0, // Top display area
                      TILES_US     = 4'd1, // Player's ships, opponents guesses area
                      TILES_THEM   = 4'd2, // Opponent's ships, players guesses area
@@ -39,12 +42,14 @@ module paint_screen(
                      ILLEGAL      = 4'hF; // Add regions if the display gets more complicated
                      
     // give names to some oft' used color codes
-    localparam [11:0] BLACK = 12'h000,
-                      WHITE = 12'hFFF,
-                      RED   = 12'hF00,
-                      BLUE  = 12'h0F0,
-                      CYAN  = 12'h0FF,
-                      GREEN = 12'h00F;
+    localparam [11:0] BLACK   = 12'h000,
+                      WHITE   = 12'hFFF,
+                      RED     = 12'hF00,
+                      BLUE    = 12'h00F,
+                      CYAN    = 12'h0FF,
+                      GREEN   = 12'h0F0,
+					  LT_CYAN = 12'h088,
+                      LT_BLUE = 12'h008;
     
     // internal signals
     reg  [3:0] display_region;
@@ -82,7 +87,9 @@ module paint_screen(
                 TOP: begin
                     // for now, just display white but this will have to index into the 
                     // screen ROM and someday maybe one of the planned status message RAMs
-                    screen_color <= WHITE;
+                    //screen_color <= WHITE;
+					screen_address <= ((pixel_y * 10'd640) + pixel_x); // -PWL this is wider than the ROM's address but it shouldn't matter (I can adjust if necessary)
+					screen_color   <= icon_data;
                     end
                 
                 TILES_US: begin 
@@ -90,20 +97,28 @@ module paint_screen(
                     // fetch what should be displayed in that tile from tile_RAM
                     // calculate the index into the proper ROM
                     // fetch the color from that ROM and pain the screen
+					// screen_color <= BLUE;
+					screen_address <= {((pixel_y - 7'd99) & 8'h1F) , (pixel_x & 8'h1F)}; // current pixel modulo 32
+                    screen_color   <= icon_data;
                     end
                     
                 TILES_THEM: begin
                     // same as above, if the tile_RAM ends up being 20x10 instead of
                     // two 10x10s then we dont even need this block
+                    // screen_color <= RED;
+					screen_address <= {((pixel_y - 7'd99) & 8'h1F) , ((pixel_x - 9'd319) & 8'h1F)}; //current pixel modulo 32
+					screen_color   <= icon_data;
                     end
                     
                 BOTTOM: begin
-                    // for now, just display white but this will have to index into 
+                    // for now, just display white but this will have to index into
                     // the bottom display/status RAMs when we implement that part of the display
+                    screen_color <= CYAN;
                     end
                 
                 ILLEGAL: begin
                     // OMG! how did we end up in this position?
+                    $WRITE("display_region seems to be an illegal value. I blame Paul!");
                     end
             endcase
         end//else
--- simulation/paint_screen_tb.v
+++ simulation/paint_screen_tb.v
@@ -1,1 +1,22 @@
-// placeholder
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// 
+// Paul Long <pwl@pdx.edu> 
+// 
+// Create Date: 12/01/2014 01:57:34 PM
+// Design Name: ece540 Final Project
+// Module Name: paint_screen_TB
+//
+// Description: 
+// 
+//
+// Revision:
+// Revision 0.01 - File Created
+
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+// For now I am just going to plug this sucker into the board and let 'er rip
+
