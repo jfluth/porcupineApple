1c553bc08aeee03f3c3fd50a25449ce33b361513
Paul Long <github@thelongs.ws>
2014-12-01 23:18:00 -0800

    Added empty tile ROM IP

---
+++ hdl/video_subsystem/empty_tile_ROM/empty_tile_ROM_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2014 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2014.3 (win64) Build 1034051 Fri Oct  3 17:14:12 MDT 2014
+-- Date        : Mon Dec 01 22:51:45 2014
+-- Host        : IPA running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub {c:/Users/pwl/Git
+--               Repos/540/final_proj/hdl/video_subsystem/empty_tile_ROM/empty_tile_ROM_stub.vhdl}
+-- Design      : empty_tile_ROM
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a100tcsg324-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity empty_tile_ROM is
+  Port ( 
+    clka : in STD_LOGIC;
+    addra : in STD_LOGIC_VECTOR ( 9 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 11 downto 0 )
+  );
+
+end empty_tile_ROM;
+
+architecture stub of empty_tile_ROM is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,addra[9:0],douta[11:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_2,Vivado 2014.3";
+begin
+end;
