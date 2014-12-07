f6ce20f8e5ba639954e3c444c0b70cd1b6516027
Paul Long <github@thelongs.ws>
2014-12-07 08:05:21 -0800

    Added some more ROMs

---
+++ hdl/video_subsystem/hit_ROM/hit_ROM_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2014 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2014.3 (win64) Build 1034051 Fri Oct  3 17:14:12 MDT 2014
+-- Date        : Sun Dec 07 01:06:16 2014
+-- Host        : IPA running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub {c:/Users/pwl/Git
+--               Repos/540/final_proj/hdl/video_subsystem/hit_ROM/hit_ROM_stub.vhdl}
+-- Design      : hit_ROM
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a100tcsg324-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity hit_ROM is
+  Port ( 
+    clka : in STD_LOGIC;
+    addra : in STD_LOGIC_VECTOR ( 9 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 11 downto 0 )
+  );
+
+end hit_ROM;
+
+architecture stub of hit_ROM is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,addra[9:0],douta[11:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_2,Vivado 2014.3";
+begin
+end;
--- hdl/video_subsystem/logo_ROM/logo_ROM_stub.vhdl
+++ hdl/video_subsystem/logo_ROM/logo_ROM_stub.vhdl
@@ -1,7 +1,7 @@
 -- Copyright 1986-2014 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2014.3 (win64) Build 1034051 Fri Oct  3 17:14:12 MDT 2014
--- Date        : Mon Dec 01 20:45:13 2014
+-- Date        : Sat Dec 06 23:50:55 2014
 -- Host        : IPA running 64-bit major release  (build 9200)
 -- Command     : write_vhdl -force -mode synth_stub {c:/Users/pwl/Git
 --               Repos/540/final_proj/hdl/video_subsystem/logo_ROM/logo_ROM_stub.vhdl}
@@ -15,7 +15,6 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity logo_ROM is
   Port ( 
     clka : in STD_LOGIC;
-    ena : in STD_LOGIC;
     addra : in STD_LOGIC_VECTOR ( 15 downto 0 );
     douta : out STD_LOGIC_VECTOR ( 11 downto 0 )
   );
@@ -26,7 +25,7 @@ architecture stub of logo_ROM is
 attribute syn_black_box : boolean;
 attribute black_box_pad_pin : string;
 attribute syn_black_box of stub : architecture is true;
-attribute black_box_pad_pin of stub : architecture is "clka,ena,addra[15:0],douta[11:0]";
+attribute black_box_pad_pin of stub : architecture is "clka,addra[15:0],douta[11:0]";
 attribute x_core_info : string;
 attribute x_core_info of stub : architecture is "blk_mem_gen_v8_2,Vivado 2014.3";
 begin
