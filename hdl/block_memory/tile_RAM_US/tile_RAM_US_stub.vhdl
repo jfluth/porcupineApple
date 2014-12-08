0d94a03ee79e0e6ed02b56a161d4b9dc4c37eeca
Paul Long <github@thelongs.ws>
2014-12-07 21:22:11 -0800

    Putting good RAMs back in

---
+++ hdl/block_memory/tile_RAM_US/tile_RAM_US_stub.vhdl
@@ -0,0 +1,39 @@
+-- Copyright 1986-2014 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2014.3 (win64) Build 1034051 Fri Oct  3 17:14:12 MDT 2014
+-- Date        : Sun Dec 07 21:12:23 2014
+-- Host        : IPA running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub {C:/Users/pwl/Git
+--               Repos/540/final_proj/hdl/block_memory/tile_RAM_US/tile_RAM_US_stub.vhdl}
+-- Design      : tile_RAM_US
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a100tcsg324-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity tile_RAM_US is
+  Port ( 
+    clka : in STD_LOGIC;
+    wea : in STD_LOGIC_VECTOR ( 0 to 0 );
+    addra : in STD_LOGIC_VECTOR ( 9 downto 0 );
+    dina : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 3 downto 0 );
+    clkb : in STD_LOGIC;
+    web : in STD_LOGIC_VECTOR ( 0 to 0 );
+    addrb : in STD_LOGIC_VECTOR ( 9 downto 0 );
+    dinb : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    doutb : out STD_LOGIC_VECTOR ( 3 downto 0 )
+  );
+
+end tile_RAM_US;
+
+architecture stub of tile_RAM_US is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,wea[0:0],addra[9:0],dina[3:0],douta[3:0],clkb,web[0:0],addrb[9:0],dinb[3:0],doutb[3:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_2,Vivado 2014.3";
+begin
+end;
