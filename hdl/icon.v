bca15cc36d20154fda02456a0df228c5e5bd3d7f
iompku <iompku@gmail.com>
2014-12-08 10:27:22 -0800

    Update icon.v
    Working on fixing bugs

--- hdl/icon.v
+++ hdl/icon.v
@@ -1,4 +1,4 @@
-\`timescale 1ns / 1ps
+`timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // 
 // Paul Long <pwl@pdx.edu> 
@@ -12,7 +12,7 @@
 //
 // Revision:
 // 	7 December 2014    PWL File Created
-//	8 December 2014	   AN File fixed (some bugs with signal not going out, tiles are still off-set)
+//	8 December 2014    AN fixing bugs
 //  
 //
 // 
@@ -39,36 +39,43 @@ module ghost_ship(
 	
 	// continuous assigns to get tile and cursor coordinates
 	wire [3:0]  tile_x   = pixel_x[8:5];
-	wire [3:0]  tile_y   = pixel_y[8:5];
+	wire [3:0]  tile_y   = pixel_y[8:5] - 10'd3;   //offset for banner
 	wire [3:0]  cursor_x = cursor[7:4];
 	wire [3:0]  cursor_y = cursor[3:0];
+	
+	wire [3:0] tile_x_min, tile_x_max, tile_y_min, tile_y_max;
+	
+	assign tile_x_min =  (cursor_x - length + 1) >= 0 ? (cursor_x - length + 1) : 0;
+	assign tile_x_max =  (cursor_x + length - 1) <= 9 ? (cursor_x + length - 1) : 9;
+	assign tile_y_min =  (cursor_y - length + 1) >= 0 ? (cursor_y - length + 1) : 0;
+	assign tile_y_max =  (cursor_y + length - 1) <= 9 ? (cursor_y + length - 1) : 9;
 	
 	
 	always @(posedge clk) begin
 		case(orientation)
 			NORTH: begin
-				if ((tile_x == cursor_x) && (tile_y >= (cursor_y - length)) && (tile_y <= cursor_y)) begin
+				if ((tile_x == cursor_x) && (tile_y >= tile_y_min) && (tile_y <= cursor_y)) begin
 					ghost_ship <= 1'b1;
 				end else begin
 					ghost_ship <= 1'b0;
 				end
 			end
 			EAST:  begin
-				if ((tile_y == cursor_y) && (tile_x >= cursor_x) && (tile_x <= cursor_x + length)) begin
+				if ((tile_y == cursor_y) && (tile_x >= cursor_x) && (tile_x <= tile_x_max)) begin
 				    ghost_ship <= 1'b1;
 				end else begin
 					ghost_ship <= 1'b0;
 				end
 			end
 			SOUTH: begin
-				if ((tile_x == cursor_x) && (tile_y >= cursor_y) && (tile_y <= cursor_y + length)) begin
+				if ((tile_x == cursor_x) && (tile_y >= cursor_y) && (tile_y <= tile_y_max)) begin
 				    ghost_ship <= 1'b1;
 				end else begin
 					ghost_ship <= 1'b0;
 				end
 			end
 			WEST:  begin
-				if ((tile_y == cursor_y) && (tile_x >= cursor_x - length) && (tile_x <= cursor_x)) begin
+				if ((tile_y == cursor_y) && (tile_x >= tile_x_min) && (tile_x <= cursor_x)) begin
 				    ghost_ship <= 1'b1;
 				end else begin
 					ghost_ship <= 1'b0;
